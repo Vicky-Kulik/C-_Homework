ea1c07f9eb86b8907bba70dc3e55076c4bd455c4
Vika Kulik <viktoriya.onegina@gmail.com>
2022-09-16 20:44:07 +0300

    Task 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача на нахождение наибольшего из двух чисел
+// введение чисел
+Console.Write("Введите первое число: ") ;
+int number1 = int.Parse(Console.ReadLine ()) ;
+
+Console.Write("Введите второе число: ") ;
+int number2 = int.Parse(Console.ReadLine ()) ;
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Наибольшее число {number1}") ;
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {number2}") ;
+}
