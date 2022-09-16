27668cc8d746f4f0d8147dfe79d91383d4063d6a
Vika Kulik <viktoriya.onegina@gmail.com>
2022-09-16 22:18:34 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿// Задача на оперделение четности числа
+﻿// Задача на вывод всех четных чисел от 1 до N
 // введение чисел
 Console.Write("Введите число: ") ;
 int num = int.Parse(Console.ReadLine ()) ;
+int result = num;
 
-if (num %2 == 0) 
-{
-    Console.WriteLine($"Число {num} четное ");
+if (num %2 == 0)
+{   
+    while (result > 0) {
+    Console.WriteLine(result);
+    result = result-2 ;
+    }
 }
 else
-{
-   Console.WriteLine($"Число {num} нечетное ") ;
+{   result = num-1;
+    while (result > 0) {
+    Console.WriteLine(result);
+    result = result-2 ;
+    }
 }
